8cb3e2fe2eeac42526a814b2a191968b9ecb7542
GohTengFong <122248517+GohTengFong@users.noreply.github.com>
2023-07-11 11:57:43 +0800

    Time: 25 ms (91.75%), Space: 42.2 MB (27.46%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -19,31 +19,31 @@ class Node {
 */
 
 class Solution {
-  public Node cloneGraph(Node node) {
-    if (node == null) {
-      return null;
-    }
-
-    HashMap<Node, Node> visited = new HashMap<>();
-    Node newNode = new Node(node.val);
-    visited.put(node, newNode);
-    Queue<Node> queue = new LinkedList<>();
-    queue.offer(node);
-
-    while (!queue.isEmpty()) {
-      Node cur = queue.poll();
-
-      List<Node> newNeighbors = visited.get(cur).neighbors;
-      for (Node n : cur.neighbors) {
-        if (!visited.containsKey(n)) {
-          visited.put(n, new Node(n.val));
-          queue.offer(n);
+    public Node cloneGraph(Node node) {
+        if (node == null) {
+        return null;
         }
 
-        newNeighbors.add(visited.get(n));
-      }
-    }
+        HashMap<Node, Node> visited = new HashMap<>();
+        Node newNode = new Node(node.val);
+        visited.put(node, newNode);
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(node);
 
-    return newNode;
-  }
+        while (!queue.isEmpty()) {
+        Node cur = queue.poll();
+
+        List<Node> newNeighbors = visited.get(cur).neighbors;
+        for (Node n : cur.neighbors) {
+            if (!visited.containsKey(n)) {
+            visited.put(n, new Node(n.val));
+            queue.offer(n);
+            }
+
+            newNeighbors.add(visited.get(n));
+        }
+        }
+
+        return newNode;
+    }
 }
