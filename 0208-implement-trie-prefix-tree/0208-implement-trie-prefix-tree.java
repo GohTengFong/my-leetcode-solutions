63d1d5093bd5d86356448d45cb3304c7fd7eecc0
GohTengFong <122248517+GohTengFong@users.noreply.github.com>
2023-07-12 12:05:20 +0800

    Time: 36 ms (96.03%), Space: 55 MB (34.71%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.java
@@ -1,43 +1,41 @@
 class Trie {
-
-        class Node {
-        Node[] nodes;
-        boolean isEnd;
-
-        Node() {
+	class Node {
+		Node[] nodes;
+		boolean isEnd;
+		
+		Node() {
             nodes = new Node[26];
-        }
-
-        private void insert(String word, int idx) {
+		}
+	
+		private void insert(String word, int idx) {
             if (idx >= word.length()) return;
             int i = word.charAt(idx) - 'a';
             if (nodes[i] == null) {
-                nodes[i] = new Node();
-            }
-
-            if (idx == word.length()-1) nodes[i].isEnd = true;
-            nodes[i].insert(word, idx+1);
-        }
-
-        private boolean search(String word, int idx) {
+            nodes[i] = new Node();
+	    }
+	
+	    if (idx == word.length()-1) nodes[i].isEnd = true;
+	        nodes[i].insert(word, idx+1);
+		}
+	
+		private boolean search(String word, int idx) {
             if (idx >= word.length()) return false;
             Node node = nodes[word.charAt(idx) - 'a'];
             if (node == null) return false;
             if (idx == word.length() - 1 && node.isEnd) return true;
-
+        
             return node.search(word, idx+1);
-
-        }
-
-        private boolean startsWith(String prefix, int idx) {
+		}
+	
+		private boolean startsWith(String prefix, int idx) {
             if (idx >= prefix.length()) return false;
             Node node = nodes[prefix.charAt(idx) - 'a'];
             if (node == null) return false;
             if (idx == prefix.length() - 1) return true;
-
+        
             return node.startsWith(prefix, idx+1);
-        }
-    }
+		}
+	}
 
     Node root;
 
