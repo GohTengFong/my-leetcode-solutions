78b44a66c5b3c04c8e090b21139ef644c91c4e1f
GohTengFong <122248517+GohTengFong@users.noreply.github.com>
2023-07-13 21:34:44 +0800

    Time: 18 ms (69.57%), Space: 43.4 MB (62.37%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,17 +1,17 @@
 class Solution {
-  public int coinChange(int[] coins, int amount) {
-    // state = amount i
-    int[] dp = new int[amount + 1]; // stores the fewest number of coins needed to make up amoount i
+    public int coinChange(int[] coins, int amount) {
+        // state = amount i
+        int[] dp = new int[amount + 1]; // stores the fewest number of coins needed to make up amoount i
 
-    for (int i = 1; i < dp.length; i++) {
-        dp[i] = dp.length;
+        for (int i = 1; i < dp.length; i++) {
+            dp[i] = dp.length;
 
-        for (int j = 0; j < coins.length; j++) {
-            // if amount i >= current coin
-            if (i >= coins[j]) dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+            for (int j = 0; j < coins.length; j++) {
+                // if amount i >= current coin
+                if (i >= coins[j]) dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
+            }
         }
+        
+        return dp[amount] == dp.length ? -1 : dp[amount];
     }
-    
-    return dp[amount] == dp.length ? -1 : dp[amount];
-  }
 }
