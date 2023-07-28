9b0ed0b3cf88607b368e4d15c08916ffceec7588
GohTengFong <122248517+GohTengFong@users.noreply.github.com>
2023-07-28 16:24:27 +0800

    Time: 0 ms (100%), Space: 40.9 MB (39.68%) - LeetHub

---
+++ 0289-game-of-life/0289-game-of-life.java
@@ -0,0 +1,65 @@
+class Solution {
+    private int[] checkNeighbours(int[][] board, int row, int col) {
+        int[] neighbours = new int[2];
+        
+        if (row - 1 >= 0) {
+            if (col + 1 < board[0].length) neighbours[board[row - 1][col + 1]] += 1;
+            if (col - 1 >= 0) neighbours[board[row - 1][col - 1]] += 1;
+
+            neighbours[board[row - 1][col]] += 1;
+        }
+
+        if (row + 1 < board.length) {
+            if (col + 1 < board[0].length) neighbours[board[row + 1][col + 1]] += 1;
+            if (col - 1 >= 0) neighbours[board[row + 1][col - 1]] += 1;
+
+            neighbours[board[row + 1][col]] += 1;
+        }
+
+        if (col - 1 >= 0) neighbours[board[row][col - 1]] += 1;
+
+        if (col + 1 < board[0].length) neighbours[board[row][col + 1]] += 1;
+
+        return neighbours;
+    }
+
+    private void updateTemp(int[][] board, int[][] temp, int row, int col, int value) {
+        int[] neighbours = checkNeighbours(board, row, col);
+
+        if (value == 0 && neighbours[1] == 3) {
+            temp[row][col] = 1;
+        } else if (value == 1) {
+            if (neighbours[1] < 2) temp[row][col] = 0;
+            else if (neighbours[1] == 2 || neighbours[1] == 3) temp[row][col] = 1;
+            else if (neighbours[1] > 3) temp[row][col] = 0;
+        }
+    }
+
+    public void gameOfLife(int[][] board) {
+        int[][] temp = new int[board.length][board[0].length];
+
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[0].length; col++) {
+                if (board[row][col] == 0) updateTemp(board, temp, row, col, 0);
+                else if (board[row][col] == 1) updateTemp(board, temp, row, col, 1);
+            }
+        }
+
+        for (int row = 0; row < board.length; row++) {
+            for (int col = 0; col < board[0].length; col++) {
+                board[row][col] = temp[row][col];
+            }
+        }
+    }
+}
+
+/*
+0 - dead
+1 - live
+
+
+1. if 1 has <2 1 neigbours, it becomes 0
+2. if 1 has 2 or 3 1 neighbours, it remains as 1
+3. if 1 has >3 1 neigbours, it becomes 0
+4. if 0 has 3 1 neigbours, it becomes 1
+*/
