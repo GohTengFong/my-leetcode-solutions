03500bee53c9c70d11744ec508fba1400db34f80
GohTengFong <122248517+GohTengFong@users.noreply.github.com>
2023-07-28 16:37:47 +0800

    Time: 7 ms (80.26%), Space: 43.9 MB (99.65%) - LeetHub

---
+++ 2038-nearest-exit-from-entrance-in-maze/2038-nearest-exit-from-entrance-in-maze.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int nearestExit(char[][] maze, int[] entrance) {
+        int rows = maze.length;
+        int columns = maze[0].length;
+            
+        int[][] directions = new int[][] {{0,1},{0,-1},{1,0},{-1,0}};
+        Queue<int[]> queue = new LinkedList<int[]>();
+        queue.offer(entrance);
+        maze[entrance[0]][entrance[1]] = '+';
+
+        int steps = 0;
+
+        while (!queue.isEmpty()) {
+            steps++;
+
+            int n = queue.size();
+            for (int i = 0; i < n; i++) {
+                int[] current = queue.poll();
+
+                for (int[] direction : directions) {
+                    int x = current[0] + direction[0];
+                    int y = current[1] + direction[1];
+
+                    if (x < 0 || x >= rows || y < 0 || y >= columns) continue;
+                    else if (maze[x][y] == '+') continue;
+                    else if (x == 0 || x == rows - 1 || y == 0 || y == columns - 1) return steps;
+
+                    maze[x][y] = '+';
+                    queue.offer(new int[] {x, y});
+                }
+            }
+        }
+
+        return -1;
+    }
+}
