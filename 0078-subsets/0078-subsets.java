d755241fe5eab4958bef4c6521593fa5f1cc02e6
GohTengFong <122248517+GohTengFong@users.noreply.github.com>
2023-08-03 09:30:20 +0800

    Time: 1 ms (70.88%), Space: 42 MB (48.56%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -7,7 +7,6 @@ class Solution {
             dfs(i + 1, nums, answer, currList);
             currList.remove(currList.size() - 1);
         }
-
     }
 
     public List<List<Integer>> subsets(int[] nums) {
