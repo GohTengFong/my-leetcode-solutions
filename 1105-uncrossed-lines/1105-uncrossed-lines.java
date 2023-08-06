4b9124311cfa33d6bc12edec578f576c52e29439
GohTengFong <122248517+GohTengFong@users.noreply.github.com>
2023-08-06 21:47:13 +0800

    Time: 5 ms (90.57%), Space: 43.8 MB (5.81%) - LeetHub

--- 1105-uncrossed-lines/1105-uncrossed-lines.java
+++ 1105-uncrossed-lines/1105-uncrossed-lines.java
@@ -1,26 +1,20 @@
 class Solution {
-    private int helper(int i, int j, int[] nums1, int[] nums2, int[][] dp) {
-        if (i == nums1.length || j == nums2.length) return 0;
+    public int maxUncrossedLines(int[] nums1, int[] nums2) {
+        int n = nums1.length;
+        int m = nums2.length;
+            
+        int[][] dp = new int[n + 1][m + 1];
 
-        if (dp[i][j] != -1) return dp[i][j];
-
-        int count = 0;
-
-        if (nums1[i] == nums2[j]) {
-            count = 1 + helper(i + 1, j + 1, nums1, nums2, dp);
-        } else {
-            count += Math.max(helper(i, j + 1, nums1, nums2, dp), helper(i + 1, j, nums1, nums2, dp));
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
         }
 
-        dp[i][j] = count;
-
-        return dp[i][j];
-    }
-
-    public int maxUncrossedLines(int[] nums1, int[] nums2) {
-        int[][] dp = new int[nums1.length][nums2.length];
-        for (int[] row : dp) Arrays.fill(row, -1);
-
-        return helper(0, 0, nums1, nums2, dp);
+        return dp[n][m];
     }
 }
