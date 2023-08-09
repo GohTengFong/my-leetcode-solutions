4e44c53e79341798cee234b36ecba123b48147ec
GohTengFong <122248517+GohTengFong@users.noreply.github.com>
2023-08-09 15:08:52 +0800

    Time: 27 ms (33.59%), Space: 41.9 MB (75.76%) - LeetHub

--- 0133-clone-graph/0133-clone-graph.java
+++ 0133-clone-graph/0133-clone-graph.java
@@ -20,30 +20,41 @@ class Node {
 
 class Solution {
     public Node cloneGraph(Node node) {
-        if (node == null) {
-        return null;
-        }
+        if (node == null) return null;
 
-        HashMap<Node, Node> visited = new HashMap<>();
-        Node newNode = new Node(node.val);
-        visited.put(node, newNode);
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(node);
+        HashMap<Node, Node> visited = new HashMap<Node, Node>(); // key = old Node, value = new Node
 
-        while (!queue.isEmpty()) {
-        Node cur = queue.poll();
+        Queue<Node> frontier1 = new LinkedList<Node>();
+        frontier1.offer(node);
 
-        List<Node> newNeighbors = visited.get(cur).neighbors;
-        for (Node n : cur.neighbors) {
-            if (!visited.containsKey(n)) {
-            visited.put(n, new Node(n.val));
-            queue.offer(n);
+        while (!frontier1.isEmpty()) {
+            int size = frontier1.size();
+            Queue<Node> frontier2 = new LinkedList<Node>();
+
+            for (int i = 0; i < size; i++) {
+                Node currNode = frontier1.poll();
+                List<Node> currNeighbours = currNode.neighbors;
+
+                // create old Node, new Node pair if it does not exist
+                if (!visited.containsKey(currNode)) {
+                    visited.put(currNode, new Node(currNode.val, new ArrayList<Node>()));
+                }
+
+                List<Node> newNeighbours = visited.get(currNode).neighbors;
+                for (Node currNeighbour : currNeighbours) {
+                    if (!visited.containsKey(currNeighbour)) {
+                        visited.put(currNeighbour, new Node(currNeighbour.val));
+                        frontier2.offer(currNeighbour);
+                    }
+
+                    newNeighbours.add(visited.get(currNeighbour));
+                }
             }
 
-            newNeighbors.add(visited.get(n));
-        }
+            frontier1 = frontier2;
         }
 
+        Node newNode = visited.get(node);
         return newNode;
     }
 }
