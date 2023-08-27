b0b2bf0a2ecb912a31b4cdd80e206b009d284b9c
GohTengFong <122248517+GohTengFong@users.noreply.github.com>
2023-08-27 21:46:05 +0800

    Time: 14 ms (8.91%), Space: 44.3 MB (6.37%) - LeetHub

---
+++ 2021-remove-all-occurrences-of-a-substring/2021-remove-all-occurrences-of-a-substring.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String removeOccurrences(String s, String part) {
+        StringBuilder sb = new StringBuilder(s);
+
+        int windowSize = part.length();
+
+        int i = 0;
+        while (i + windowSize <= sb.length()) {
+            if (sb.substring(i, windowSize + i).equals(part)) {
+                sb.delete(i, windowSize + i);
+                System.out.println(sb.toString());
+                i = 0;
+            } else {
+                i++;
+            }
+        }
+
+        return sb.toString();
+    }
+}
