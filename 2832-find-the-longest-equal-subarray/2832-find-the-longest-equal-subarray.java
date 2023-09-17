3cc245fbb5476d5d36a65ffa1cd571ae9ff6e08c
GohTengFong <122248517+GohTengFong@users.noreply.github.com>
2023-09-17 22:33:23 +0800

    Time: 76 ms (78.89%), Space: 62.9 MB (50.44%) - LeetHub

---
+++ 2832-find-the-longest-equal-subarray/2832-find-the-longest-equal-subarray.java
@@ -0,0 +1,18 @@
+class Solution {
+	public int longestEqualSubarray(List<Integer> nums, int k) {
+		int maxf = 0; 
+		int i = 0;
+		int n = nums.size();
+		Map<Integer, Integer> count = new HashMap<Integer, Integer>();
+		
+		for (int j = 0; j < n; j++) {
+		    count.put(nums.get(j), count.getOrDefault(nums.get(j), 0) + 1);
+		    maxf = Math.max(maxf, count.get(nums.get(j)));
+		    if (j - i + 1 - maxf > k) {
+		        count.put(nums.get(i), count.get(nums.get(i)) - 1);
+		        i++;
+		    }
+		}
+		return maxf;
+	}
+}
