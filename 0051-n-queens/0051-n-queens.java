9a874b115034b2926f4d939cde4e1c86ca2c1037
GohTengFong <122248517+GohTengFong@users.noreply.github.com>
2023-09-20 22:06:00 +0800

    Time: 4 ms (49.48%), Space: 43.6 MB (87.02%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,10 +1,11 @@
 class Solution {
+	// records a possible solution 
     private void recordState(ArrayList<List<String>> answer, int[][] board) {
         ArrayList<String> boardInList = new ArrayList<String>();
         
         for (int row = 0; row < board.length; row++) {
             StringBuilder state = new StringBuilder();
-            
+        
             for (int col = 0; col < board[0].length; col++) {
                 if (board[row][col] == 0) state.append('.');
                 else state.append('Q');
@@ -15,7 +16,8 @@ class Solution {
 
         answer.add(boardInList);
     }
-
+	
+	// checks if it is possible to place a queen at board[row][col]
     private boolean canPut(int[][] board, int row, int col) {
         for (int i = row; i < board.length; i++) { // checking for all rows >= row
             if (board[i][col] == 1) return false;
@@ -64,21 +66,21 @@ class Solution {
         }
 
         return true;
-    }
+  }
 
     private void dfs(ArrayList<List<String>> answer, int remainingQ, int[][] board, int row) {
-        if (remainingQ == 0) {
+        if (remainingQ == 0) { // found a possible solution, add it to answer
             recordState(answer, board);
         }
 
-        if (row >= board.length) return;
+        if (row >= board.length) return; // remainingQ != 0 and exceeding board, not a possible solution
 
         int[] nextRow = board[row];
-        for (int col = 0; col < board[0].length; col++) {
-            if (canPut(board, row, col)) {
-                nextRow[col] = 1;
-                dfs(answer, remainingQ - 1, board, row + 1);
-                nextRow[col] = 0;
+        for (int col = 0; col < board[0].length; col++) { // iterate through all the columns of nextRow
+            if (canPut(board, row, col)) { // found a possible space for a queen
+                nextRow[col] = 1; // set the queen
+                dfs(answer, remainingQ - 1, board, row + 1); // recursively explore nextRow + 1
+                nextRow[col] = 0; // backtrack, unset the queen
             }
         }
 
@@ -89,16 +91,32 @@ class Solution {
         ArrayList<List<String>> answer = new ArrayList<List<String>>();
         int[][] board = new int[n][n];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++) { // iterating through all the columns in the first row
             int[] firstRow = board[0];
             
-            firstRow[i] = 1;
+            firstRow[i] = 1; // set the first queen
 
             dfs(answer, n - 1, board, 1);
 
-            firstRow[i] = 0;
+            firstRow[i] = 0; // backtrack, unset the first queen
         }
 
         return answer;
     }
 }
+
+/*
+
+Idea :
+- brute force, for every space, try to put a queen
+- starting from the first row, iterate through the columns
+	- put a queen at board[row][col]
+		- can be sure that it is possible since it is the first queen on the board
+- recurse on nextRow
+	- iterate through the columns of the next row
+	- check if it is possible to put a queen at board[nextRow][col]
+		- if it is possible, continue recursion on nextRow
+		- if not possible, return
+- must backtrack
+
+*/
