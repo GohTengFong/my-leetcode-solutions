34f9e03609f531843a54c8fa941fe8aac0e6ed2e
GohTengFong <122248517+GohTengFong@users.noreply.github.com>
2023-09-22 20:52:35 +0800

    Time: 0 ms (100%), Space: 40.5 MB (51.15%) - LeetHub

--- 1369-minimum-swaps-to-make-strings-equal/1369-minimum-swaps-to-make-strings-equal.java
+++ 1369-minimum-swaps-to-make-strings-equal/1369-minimum-swaps-to-make-strings-equal.java
@@ -43,3 +43,29 @@ class Solution {
         return (swaps == 0) ? -1 : swaps;
     }
 }
+
+/*
+
+Idea :
+- 2 main types of mismatch
+- to swap as little as possible, resolve type 2 first
+- iterate through s1 and s2 and stop when letters are not matching
+	- iterate through the remaining letters in s1 and s2 to find another letters mismatch and s1Char[i] == s1Char[j]
+		- this indicates a type 2
+			- swap letters and increment swap by 1
+	- if there are no such letter mismatch where s1Char[i] == s1Char[j]
+		- this indicates that there are no more type 2, must look for type 1
+		- iterate through the remaining letters in s1 and s2 to find another letter mismatch and s1Char[i] != s1Char[j]
+			- this indicates a type 1
+				- swap letters and increment swap by 2
+  - if both mismatch types are unfound, no answer is possible, return -1
+
+Mismatch Type 1 (2 swaps required)
+s1 = xy
+s2 = yx
+
+Mismatch Type 2 (1 swap required)
+s1 = xx
+s2 = yy
+
+*/
