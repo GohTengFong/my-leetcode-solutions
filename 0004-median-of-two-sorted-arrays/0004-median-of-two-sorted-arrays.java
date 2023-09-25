1008021fd47486a101964a15943ebae6f47060f2
GohTengFong <122248517+GohTengFong@users.noreply.github.com>
2023-09-25 19:33:34 +0800

    Time: 9 ms (9.03%), Space: 44.5 MB (75.1%) - LeetHub

---
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -0,0 +1,54 @@
+class Solution {
+    private void combineArrays(int[] nums1, int[] nums2, int[] combined) {
+        int oneIndex = 0;
+        int twoIndex = 0;
+        int combinedIndex = 0;
+
+        while (oneIndex < nums1.length || twoIndex < nums2.length) {
+            if (oneIndex >= nums1.length) {
+                combined[combinedIndex] = nums2[twoIndex];
+                twoIndex++;
+                combinedIndex++;
+                continue;
+            }
+
+            if (twoIndex >= nums2.length) {
+                combined[combinedIndex] = nums1[oneIndex];
+                oneIndex++;
+                combinedIndex++;
+                continue;                
+            }
+
+            if (nums1[oneIndex] <= nums2[twoIndex]) {
+                combined[combinedIndex] = nums1[oneIndex];
+                oneIndex++;
+                combinedIndex++;
+            } else {
+                combined[combinedIndex] = nums2[twoIndex];
+                twoIndex++;
+                combinedIndex++;
+            }
+        }
+    }
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int length = nums1.length + nums2.length;
+        int[] combined = new int[length];
+        combineArrays(nums1, nums2, combined);
+
+        System.out.println(Arrays.toString(combined));
+
+        if (combined.length % 2 == 0) {
+            int right = length / 2;
+            int left = right - 1;
+
+            double answer = ((double) combined[left] + combined[right]) / 2;
+
+            return answer;
+        } else {
+            int index = length / 2;
+
+            return combined[index];
+        }
+    }
+}
