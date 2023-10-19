d08d13343845a5df40cc5b2774ed3de67a9ea63d
GohTengFong <122248517+GohTengFong@users.noreply.github.com>
2023-10-19 10:14:38 +0800

    Time: 38 ms (62.24%), Space: 46.7 MB (46.83%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -7,25 +7,30 @@ class Solution {
 
         int target = sum / 2; // the target sum for each half of the partition
 
-        HashSet<Integer> set1 = new HashSet<Integer>(); // stores every possible sum of the subarray starting from the back
-        set1.add(0);
+        // dp[i][j] represents if it is possible to form target = j with any subset of items (0 - i)
+        boolean[][] dp = new boolean[nums.length + 1][target + 1];
+        dp[0][0] = true;
 
-        // iterate through nums starting from the back
-        for (int i = nums.length - 1; i >= 0; i--) {
-            int currNum = nums[i];
-            HashSet<Integer> set2 = new HashSet<Integer>(); // used to store the updated sum of subarray starting from the back, prevents set1 from iterating over itself
-
-            for (Integer value : set1) {
-                if (currNum + value == target) return true; // if there is a subarray sum + nums[i] that equals target that means that it is possible to partition nums
-
-                // else add to set2
-                set2.add(value); 
-                set2.add(nums[i] + value);
-            }
-
-            set1 = set2; // replace set1 with set2
+        for (int row = 0; row <= nums.length; row++) {
+            dp[row][0] = true; // can always form target = 0 with i items (empty subset)
         }
 
-        return false;
+        for (int col = 1; col <= target; col++) {
+            dp[0][col] = false; // cannot form target = j with i = 0 items
+        }
+
+        for (int i = 1; i <= nums.length; i++) {
+            for (int j = 1; j <= target; j++) {
+                if (nums[i - 1] <= j) { // incoming item may or may not be included
+                    // dp[i - 1][j] represents excluding incoming item
+                    // dp[i - 1][j - nums[i - 1]] represents including incoming item
+                    dp[i][j] = (dp[i - 1][j] || dp[i - 1][j - nums[i - 1]]);
+                } else { // incoming item must be excluded
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[nums.length][target];
     }
 }
